cbf480574c101f24f7406d51e1f2cd090b1dc5b6
mariaizabeldsantos <102763149+mariaizabeldsantos@users.noreply.github.com>
2022-04-15 23:40:56 -0300

    Add files via upload

---
+++ nome.py
@@ -0,0 +1,7 @@
+nome = input("Insira o seu nome: ")
+
+salario = input("Salário do mês de abril: ")
+
+informaçoes = "O salário de" + " " + nome + " " + "no mês de abril foi de R$"  + salario + ",00 reais"
+
+print(informaçoes)
